31d61c092d009f0644a86b182605942a8febeacd
明远湖之鱼 <488548816@qq.com>
2023-06-25 23:58:08 +0800

    feat: 获取好友列表组件开发

--- client/src/pages/container/AddressBook/index.tsx
+++ client/src/pages/container/AddressBook/index.tsx
@@ -1,5 +1,6 @@
-import { message, Tabs } from 'antd';
 import { useEffect, useState } from 'react';
+import { message, Tabs, Tree } from 'antd';
+import type { DataNode } from 'antd/es/tree';
 
 import styles from './index.module.less';
 
@@ -8,6 +9,20 @@ import { IFriendGroup } from './api/type';
 
 const AddressBook = () => {
   const [friendList, setFriendList] = useState<IFriendGroup[]>([]); // 好友列表
+
+  // 难点: 如何将后端返回的数据转换成Tree组件需要的数据格式
+  const treeData = friendList.map((group) => {
+    return {
+      title: group.name,
+      key: String(Math.random()), // 根据实际情况生成唯一的 key，这里简单使用了随机数
+      selectable: false,
+      children: group.friend.map((friend) => ({
+        title: friend.remark,
+        key: String(friend.id),
+        // 其他属性可以根据需要自行添加
+      })),
+    };
+  });
 
   useEffect(() => {
     getFriendList().then((res) => {
@@ -23,7 +38,7 @@ const AddressBook = () => {
     <div className={styles.addressBookTabs}>
       <Tabs centered>
         <Tabs.TabPane tab="好友" key="1">
-          好友
+          <Tree treeData={treeData} />
         </Tabs.TabPane>
         <Tabs.TabPane tab="群聊" key="2">
           群聊
