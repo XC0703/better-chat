5d85c7e3fcdf1dc4d01222b6ca44280d675d62df
明远湖之鱼 <488548816@qq.com>
2023-07-07 00:06:37 +0800

    feat: 好友信息展示组件样式编写

--- client/src/pages/container/AddressBook/index.tsx
+++ client/src/pages/container/AddressBook/index.tsx
@@ -1,7 +1,8 @@
-import { Tabs, Tree, Tooltip, TabsProps, App, Form, Input, Select } from 'antd';
+import { Tabs, Tree, Tooltip, TabsProps, App, Form, Input, Select, Button } from 'antd';
 import type { DirectoryTreeProps } from 'antd/es/tree';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
+import { WechatOutlined } from '@ant-design/icons';
 import { statusIconList } from '@/assets/icons';
 import SearchContainer from '@/components/SearchContainer';
 
@@ -14,7 +15,7 @@ const AddressBook = () => {
   const { message } = App.useApp();
   const [friendList, setFriendList] = useState<IFriendGroup[]>([]); // 好友列表
   const [infoChangeInstance] = Form.useForm<{ newRemark: string; newGroup: string }>();
-  const curFriendValueRef = useRef<IFriendInfo>();
+  const [curFriendInfo, setCurFriendInfo] = useState<IFriendInfo>(); // 当前选中的好友信息
 
   // 难点: 如何将后端返回的数据转换成Tree组件需要的数据格式
   const treeData = friendList.map((group) => {
@@ -51,12 +52,11 @@ const AddressBook = () => {
   const getNodeInfoById = (id: number) => {
     getFriendInfoById(id).then((res) => {
       if (res.code === 200 && res.data) {
-        curFriendValueRef.current = res.data;
+        setCurFriendInfo(res.data);
         infoChangeInstance?.setFieldsValue({
           newRemark: res.data.remark,
           newGroup: res.data.group_name,
         });
-        console.log(curFriendValueRef.current);
       } else {
         message.error('获取好友信息失败', 1.5);
       }
@@ -100,38 +100,47 @@ const AddressBook = () => {
       children: <>群聊</>,
     },
   ];
+  // 用useMemo包裹，避免每次都重新渲染导致展开的好友列表收起
+  const LeftContainer = useMemo(() => {
+    return (
+      <div className={styles.leftContainer}>
+        <div className={styles.search}>
+          <SearchContainer />
+        </div>
+        <div className={styles.list}>
+          <div className={styles.addressBookTabs}>
+            <Tabs centered defaultActiveKey="1" items={items}></Tabs>
+          </div>
+        </div>
+      </div>
+    );
+  }, [friendList]);
   return (
     <>
       <div className={styles.addressBook}>
-        <div className={styles.leftContainer}>
-          <div className={styles.search}>
-            <SearchContainer />
-          </div>
-          <div className={styles.list}>
-            <div className={styles.addressBookTabs}>
-              <Tabs centered defaultActiveKey="1" items={items}></Tabs>
-            </div>
-          </div>
-        </div>
+        {LeftContainer}
         <div className={styles.rightContainer}>
-          {
+          {curFriendInfo === undefined ? (
+            <WechatOutlined />
+          ) : (
             <div className={styles.infoModal}>
               <div className={styles.infoContainer}>
                 <div className={styles.avatar}>
-                  <img src={curFriendValueRef.current?.avatar} alt="" />
+                  <img src={curFriendInfo?.avatar} alt="" />
                 </div>
                 <div className={styles.info}>
-                  <div className={styles.username}>{curFriendValueRef.current?.username}</div>
+                  <div className={styles.username}>{curFriendInfo?.username}</div>
                   <div className={styles.signature}>
-                    {curFriendValueRef.current?.signature === ''
-                      ? '暂无个性签名'
-                      : curFriendValueRef.current?.signature}
+                    {curFriendInfo?.signature === '' ? '暂无个性签名' : curFriendInfo?.signature}
                   </div>
                 </div>
               </div>
               <div className={styles.changeContainer}>
                 <Form form={infoChangeInstance}>
-                  <Form.Item label="账号" name="username" rules={[{ required: true }]}>
+                  <Form.Item label="账号" name="username">
+                    <Input />
+                  </Form.Item>
+                  <Form.Item label="昵称" name="username">
                     <Input />
                   </Form.Item>
                   <Form.Item label="备注" name="newRemark">
@@ -142,14 +151,33 @@ const AddressBook = () => {
                   </Form.Item>
                   <Form.Item label="分组" name="newGroup">
                     <Select
+                      size="small"
+                      notFoundContent="暂无分组"
                       placeholder="请选择分组"
                       onChange={(e) => infoChangeInstance.setFieldsValue({ newGroup: e.target.value })}
                     />
                   </Form.Item>
                 </Form>
               </div>
+              <div className={styles.btns}>
+                <Button
+                  onClick={() => {
+                    console.log('发送消息');
+                  }}
+                >
+                  发送消息
+                </Button>
+                <Button
+                  type="primary"
+                  onClick={() => {
+                    console.log('保存信息');
+                  }}
+                >
+                  保存信息
+                </Button>
+              </div>
             </div>
-          }
+          )}
         </div>
       </div>
     </>
